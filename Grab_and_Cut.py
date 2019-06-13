7bd91128d84cdc929a47e2e0e3bf7d73bbe705a1
Marisa Roldan <34078523+Muhrisa@users.noreply.github.com>
2019-06-13 11:30:31 -0700

    Add files via upload

---
+++ Grab_and_Cut.py
@@ -0,0 +1,109 @@
+import numpy as np
+import imutils
+import cv2
+
+def findSignificantContour(edgeImg):
+    image, contours, hierarchy = cv2.findContours(
+        edgeImg,
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+        # Find level 1 contours
+    level1Meta = []
+    for contourIndex, tupl in enumerate(hierarchy[0]):
+        # Each array is in format (Next, Prev, First child, Parent)
+        # Filter the ones without parent
+        if tupl[3] == -1:
+            tupl = np.insert(tupl.copy(), 0, [contourIndex])
+            level1Meta.append(tupl)
+            # From among them, find the contours with large surface area.
+    contoursWithArea = []
+    for tupl in level1Meta:
+        contourIndex = tupl[0]
+        contour = contours[contourIndex]
+        area = cv2.contourArea(contour)
+        contoursWithArea.append([contour, area, contourIndex])
+		
+    contoursWithArea.sort(key=lambda meta: meta[1], reverse=True)
+    largestContour = contoursWithArea[0][0]
+    return largestContour
+
+def filterOutSaltPepperNoise(edgeImg):
+    # Get rid of salt & pepper noise.
+    count = 0
+    lastMedian = edgeImg
+    median = cv2.medianBlur(edgeImg, 3)
+    while not np.array_equal(lastMedian, median):
+        # get those pixels that gets zeroed out
+        zeroed = np.invert(np.logical_and(median, edgeImg))
+        edgeImg[zeroed] = 0
+
+        count = count + 1
+        if count > 70:
+            break
+        lastMedian = median
+        median = cv2.medianBlur(edgeImg, 3)
+
+#print(image_to_array("testing1.png"))
+def image_change(pic):
+    src = cv2.imread(pic, 1)
+    src = imutils.resize(src, width=300)
+
+    blurred = cv2.GaussianBlur(src, (5, 5), 0)
+    blurred_float = blurred.astype(np.float32) / 255.0
+    edgeDetector = cv2.ximgproc.createStructuredEdgeDetection("model.yml")
+    edges = edgeDetector.detectEdges(blurred_float) * 255.0
+    cv2.imwrite('edge-raw.jpg', edges)
+
+    edges_8u = np.asarray(edges, np.uint8)
+    filterOutSaltPepperNoise(edges_8u)
+    cv2.imwrite('edge.jpg', edges_8u)
+
+
+    contour = findSignificantContour(edges_8u)
+    # Draw the contour on the original image
+    contourImg = np.copy(src)
+    cv2.drawContours(contourImg, [contour], 0, (0, 255, 0), 2, cv2.LINE_AA, maxLevel=1)
+    cv2.imwrite('contour.jpg', contourImg)
+    mask = np.zeros_like(edges_8u)
+    cv2.fillPoly(mask, [contour], 255)
+
+    # calculate sure foreground area by dilating the mask
+    mapFg = cv2.erode(mask, np.ones((5, 5), np.uint8), iterations=10)
+
+    # mark inital mask as "probably background"
+    # and mapFg as sure foreground
+    trimap = np.copy(mask)
+    trimap[mask == 0] = cv2.GC_BGD
+    trimap[mask == 255] = cv2.GC_PR_BGD
+    trimap[mapFg == 255] = cv2.GC_FGD
+
+    # visualize trimap
+    trimap_print = np.copy(trimap)
+    trimap_print[trimap_print == cv2.GC_PR_BGD] = 128
+    trimap_print[trimap_print == cv2.GC_FGD] = 255
+    cv2.imwrite('trimap.png', trimap_print)
+    # run grabcut
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    rect = (0, 0, mask.shape[0] - 1, mask.shape[1] - 1)
+    cv2.grabCut(src, trimap, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+
+    # create mask again
+    mask2 = np.where(
+        (trimap == cv2.GC_FGD) | (trimap == cv2.GC_PR_FGD),
+        255,
+        0
+    ).astype('uint8')
+    cv2.imwrite('mask2.jpg', mask2)
+
+    #http://enthusiaststudent.blogspot.com/2015/01/horizontal-and-vertical-flip-using.html
+
+    img=cv2.imread('mask2.jpg')
+    fimg=img.copy()
+    fimg=cv2.flip(img,0)
+    cv2.imwrite('bw_flipped.jpg', fimg)
+    return cv2.flip(src,0)
+
+    #print(image_to_array("testing1.png"))
+
---
+++ colors.py
@@ -0,0 +1,57 @@
+'''
+white (221, 221, 221)
+orange (219, 125, 62)
+magenta (179, 80, 188)
+light blue (107, 138, 201)
+yellow (177, 166, 39)
+lime (65, 174, 56)
+pink(208, 132, 153)
+grey (64, 64, 64)
+silver (154, 161, 161)
+cyan (46, 110, 137)
+purple (126, 61, 181)
+blue (46, 56, 141)
+brown (79, 50, 31)
+green (53, 70, 27)
+red (150, 52, 48)
+black (25, 22, 22)
+other blocks:
+netherrack (139, 81, 81)
+sponge (193, 193, 63)
+red_mushroom (181, 29,27)
+obsidian (38, 32, 53)
+gravel (135, 127, 127)
+gold_block (255, 245, 77)
+diamond_block (113, 225, 220)
+soul_sand (70, 51, 40)
+snow_layer / snow ??? (240, 252, 252) // this might need more fiddling with
+iron_block (235, 235, 235)
+clay (162, 168, 181)
+sand_stone (219, 212, 162) // pretty much the same as sand
+wood (92, 73, 44)
+lapis_block (26, 70, 172)
+stone 120, 120, 120
+melon_block 133, 139, 35
+double_stone_slab 163, 163, 162
+brick_block 145, 98, 85 // this is the red brick block
+stonebrick 122, 122, 122
+mossy_cobblestone 100, 120, 100 // questionable
+redstone_block 139, 22, 7
+quartz_block 215, 215, 206
+planks 156, 127, 78
+dirt 132, 95, 66
+bedrock 84, 84, 84
+glowstone 141, 116, 67
+lit_pumpkin 183, 139, 33
+noteblock 114, 73, 51
+'''
+
+woolList = ['WHITE', 'ORANGE', 'MAGENTA', 'LIGHT_BLUE', 'YELLOW', 'LIME', 'PINK', 'GRAY', 'SILVER', 'CYAN', 'PURPLE', 'BLUE', 'BROWN', 'RED', 'BLACK', 'GREEN']
+
+colorDict = {'WHITE': (221,221,221), 'ORANGE':(219,125,62),'MAGENTA':(179,80,188),'LIGHT_BLUE':(107,138,201),'YELLOW': (177,166,39), 'LIME': (65,174,56),'PINK':(208,132,153),'GRAY':(64,64,64),'SILVER': (154,161,161),
+       'CYAN': (46,110,137),'PURPLE':(126,61,181),'BLUE':(46, 56, 141),'BROWN': (79, 50, 31), 'GREEN': (53, 70, 27),'RED':(150, 52, 48),'BLACK':(25, 22, 22),'netherrack': (139, 81, 81),
+        'sponge': (193, 193, 63),'red_mushroom_block': (181, 29,27),'obsidian': (38, 32, 53),'gold_block': (255, 245, 77),'diamond_block': (113, 225, 220),'soul_sand': (70, 51, 40),
+        'snow':(240, 252, 252),'iron_block': (235, 235, 235),'clay':(162, 168, 181),'sandstone': (219, 212, 162),'lapis_block': (26, 70, 172), 'stone': (120, 120, 120),
+        'melon_block': (133, 139, 35), 'double_stone_slab': (163, 163, 162), 'brick_block': (145, 98, 85), 'mossy_cobblestone': (100, 120, 100), 'redstone_block': (139, 22, 7),
+        'quartz_block': (215, 215, 206), 'planks': (156, 127, 78), 'dirt': (132, 95, 66), 'bedrock': (84, 84, 84), 'glowstone': (141, 116, 67), 'lit_pumpkin': (183, 139, 33),
+        'noteblock': (114, 73, 51)}
